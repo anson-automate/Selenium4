8ed99e29be9ab644d62e413d53a7ee25737cf09a
anson-automate <anson.paul.p@gmail.com>
2022-10-04 21:11:42 +0530

    command and comments

--- src/test/java/org/anson/Selenium4/TestRunnerBasic.java
+++ src/test/java/org/anson/Selenium4/TestRunnerBasic.java
@@ -6,6 +6,7 @@ import java.awt.Window;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,32 +23,56 @@ public class TestRunnerBasic {
 		WebDriver driver = new ChromeDriver();
 		String url = "https://www.google.com/";
 		driver.navigate().to(url);
-
-		String mainWindow = driver.getWindowHandle();
-
-		System.out.println(mainWindow);
-
-		driver.switchTo().newWindow(WindowType.TAB);
-
-		url = "https://www.google.com/";
-		driver.navigate().to(url);
-
-		String secondWindow = driver.getWindowHandle();
-
-		System.out.println(secondWindow);
-
-		driver.switchTo().window(mainWindow);
-
+		
+		//1. Get current URL
+		
+		System.out.println("current URL "+driver.getCurrentUrl());
+		
+		// 2. Get Title
+		
+		System.out.println("Get Title" +driver.getTitle());
+		
+		// 3. Forward | Back | Refresh
+		
 		driver.navigate().back();
 		
 		driver.navigate().forward();
 		
-		driver.findElement(By.xpath("//*[@class='gLFyf gsfi']")).sendKeys("test");
-		
-		driver.findElement(By.xpath("//*[@class='gLFyf gsfi']")).sendKeys(Keys.ENTER);		
 		driver.navigate().refresh();
+		
+		// 4. Switching windows (window handle)
 
-		// driver.close();
+		String mainWindow = driver.getWindowHandle();
+
+		System.out.println(mainWindow);
+		
+		// 5. Open new Tab
+		driver.switchTo().newWindow(WindowType.TAB);
+		
+		// 6. Open new window
+		driver.switchTo().newWindow(WindowType.WINDOW);
+
+		url = "https://www.google.com/";
+		driver.navigate().to(url);
+		
+
+		String secondWindow = driver.getWindowHandle();
+
+		System.out.println(secondWindow);
+		
+		// 7. switching windows
+
+		driver.switchTo().window(mainWindow);
+		// 8. webelement 
+		WebElement search = driver.findElement(By.xpath("//*[@class='gLFyf gsfi']"));
+		// 9. sending text
+		search.sendKeys("test");
+		// 10. sending keys
+		search.sendKeys(Keys.ENTER);		
+
+		driver.close();
+		
+		driver.quit();
 
 	}
 
